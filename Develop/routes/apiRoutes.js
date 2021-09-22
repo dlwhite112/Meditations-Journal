841bbb12774466657fef0a214a19b4d128f9860b
D.L. White - Vunte Ron <dlwhite112@gmail.com>
2021-09-22 18:13:55 -0500

    adding note delete funct.

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,6 +2,8 @@
 const generateUniqueId = require('generate-unique-id');
 const router = require('express').Router();
 const fs = require('fs');
+var savedNotes;
+
 
 router.get("/notes", (req, res) => {
     fs.readFile(`${__dirname}/../db/db.json`, (err, data) => {
@@ -10,6 +12,21 @@ router.get("/notes", (req, res) => {
         res.end(data);
     })
 });
+
+router.delete("/notes/:id", (req, res) => {
+    // savedNotes = savedNotes.filter(function(data) {
+    //     console.log(`Lets See if this data.id is actually the correct ${data.id}`)
+    //   return data.id != req.params.id;
+    // });
+    // console.log("Deleting note with id " + req.params.id);
+    fs.readFile(`${__dirname}/../db/db.json`, (err, data) => {
+        if (err) throw err;
+        // res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    })
+
+    return res.json(true);
+  })
 
 // fs writeFile to read contents of my dbData (add above)
 router.post("/notes", (req, res) => {
@@ -21,8 +38,7 @@ router.post("/notes", (req, res) => {
     fs.readFile(`${__dirname}/../db/db.json`, (err, data) => {
         if (err) throw err;
 
-        let savedNotes = JSON.parse(data);
-        console.log(savedNotes);
+        savedNotes = JSON.parse(data);
         savedNotes.push(newNote);
         fs.writeFile(`${__dirname}/../db/db.json`, JSON.stringify(savedNotes), (err, data) => {
             if (err) throw err;
